fedef569ae1278c43aaa3e43384b3339f8ece8cc
Tam Nguyen <tam.nguyen@asdnh.org>
2022-02-17 20:13:56 -0500

    commit with previous code and lidar

---
+++ src/main/cpp/commands/DriveTeleop.cpp
@@ -0,0 +1,87 @@
+#include "commands/DriveTeleop.h"
+
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <iostream>
+#include <cmath>
+
+DriveTeleop::DriveTeleop(DriveSubsystem* driveSubsystem) :
+ m_driveSubsystem(driveSubsystem),
+ m_heading(0_rad){
+     AddRequirements(m_driveSubsystem);
+}
+
+void DriveTeleop::Initialize(){
+    m_heading = 0_deg;
+    m_driveSubsystem->TankDrive(0.0, 0.0);
+    m_timer.Start();
+    m_driveSubsystem->ZeroEncoders();
+    m_lastExecute = m_timer.Get();
+}
+
+void DriveTeleop::Execute(){
+    //calculate delta time
+    units::second_t currentTime = m_timer.Get();
+    units::second_t deltaTime = currentTime - m_lastExecute;
+    m_lastExecute = currentTime;
+
+    //Up should be +1 and right should be 1
+    double leftStickY = -m_driveJoystick.GetRawAxis(LEFTSTICK_Y);
+    double rightStickX = -m_driveJoystick.GetRawAxis(RIGHTSTICK_X);
+
+    //deadzones
+    if(std::abs(leftStickY) <= 0.0001){
+        leftStickY = 0.0;
+    }
+
+    if(std::abs(rightStickX) <= 0.0001){
+        rightStickX = 0.0;
+    }
+
+    //scale input based on enum
+    switch(DRIVETRAIN_SCALING){
+        case Scaling::QUADRATIC:
+            //(sign) x^2
+            leftStickY = std::pow(leftStickY, 3.0) / std::abs(leftStickY);
+            rightStickX = std::pow(rightStickX, 3.0) / std::abs(rightStickX);
+            break;
+        case Scaling::CUBIC:
+            // x^3
+            leftStickY = std::pow(leftStickY, 3.0);
+            rightStickX = std::pow(rightStickX, 3.0);
+            break;
+        case Scaling::ROOT:
+            // x * root x
+            leftStickY = leftStickY * std::sqrt(std::abs(leftStickY));
+            rightStickX = rightStickX * std::sqrt(std::abs(rightStickX));
+            break;
+    }
+        
+    rightStickX *= TURN_PERCENT;
+
+    //update heading (use if gyro mounted)
+    m_heading += rightStickX * TURN_RATE * deltaTime;
+
+    double leftSide = leftStickY - rightStickX;
+    double rightSide = leftStickY + rightStickX;
+    //prevent from going out of -1 to 1 range
+    if(rightSide > 1.0){
+        double delta = rightSide - 1.0;
+        leftSide -= delta;
+        rightSide -= delta;
+    }else if(leftSide < -1.0){
+        double delta = leftSide + 1.0;
+        leftSide += delta;
+        rightSide += delta;
+    }
+
+    m_driveSubsystem->TankDrive(leftSide, rightSide);
+}
+
+void DriveTeleop::End(bool interrupted){
+    m_timer.Stop();
+    m_driveSubsystem->TankDrive(0.0, 0.0);
+}
+
+bool DriveTeleop::IsFinished(){
+    return false;
+}
---
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -0,0 +1,125 @@
+#include "subsystems/DriveSubsystem.h"
+
+#include <wpi/numbers>
+
+DriveSubsystem::DriveSubsystem(){
+    ConfigureDefault();
+}
+
+void DriveSubsystem::Periodic(){
+
+}
+
+void DriveSubsystem::TankDrive(double left, double right){
+    m_frontLeft.Set(ControlMode::PercentOutput, left);
+    m_frontRight.Set(ControlMode::PercentOutput, right);
+}
+
+void DriveSubsystem::ConfigureDefault(){
+    //reset everything
+    m_frontLeft.ConfigFactoryDefault();
+    m_frontRight.ConfigFactoryDefault();
+    m_backLeft.ConfigFactoryDefault();
+    m_backRight.ConfigFactoryDefault();
+
+    //back motors follow front motors
+    m_backLeft.Follow(m_frontLeft);
+    m_backRight.Follow(m_frontRight);
+
+    //make the motors go in the arbitrary forward direction
+    m_frontLeft.SetInverted(true);
+    m_backLeft.SetInverted(true);
+    m_frontRight.SetInverted(false);
+    m_backRight.SetInverted(false);
+
+    //set the sensor type to an integrated sensor (falcon 500s have integrated)
+    m_frontLeft.ConfigSelectedFeedbackSensor(FeedbackDevice::IntegratedSensor, 0, 10);
+    m_frontRight.ConfigSelectedFeedbackSensor(FeedbackDevice::IntegratedSensor, 0, 10);
+
+    //10 ms period
+    m_frontLeft.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic, 10, 10);
+    m_frontRight.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic, 10, 10);
+    m_frontLeft.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0, 10, 10);
+    m_frontRight.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0, 10, 10);
+
+    //nominal = default, peak = maximum
+    m_frontLeft.ConfigNominalOutputForward(0.0, 10);
+    m_frontLeft.ConfigNominalOutputReverse(0.0, 10);
+    m_frontLeft.ConfigPeakOutputForward(1.0, 10);
+    m_frontLeft.ConfigPeakOutputReverse(-1.0, 10);
+    m_frontRight.ConfigNominalOutputForward(0.0, 10);
+    m_frontRight.ConfigNominalOutputReverse(0.0, 10);
+    m_frontRight.ConfigPeakOutputForward(1.0, 10);
+    m_frontRight.ConfigPeakOutputReverse(-1.0, 10);
+
+    //configure the distance PID values
+    m_frontLeft.SelectProfileSlot(0, 0);
+    m_frontLeft.Config_kP(0, DRIVETRAIN_DISTANCE_P);
+    m_frontLeft.Config_kI(0, DRIVETRAIN_DISTANCE_I);
+    m_frontLeft.Config_kD(0, DRIVETRAIN_DISTANCE_D);
+    m_frontLeft.Config_kF(0, DRIVETRAIN_DISTANCE_F);
+    m_frontRight.SelectProfileSlot(0, 0);
+    m_frontRight.Config_kP(0, DRIVETRAIN_DISTANCE_P);
+    m_frontRight.Config_kI(0, DRIVETRAIN_DISTANCE_I);
+    m_frontRight.Config_kD(0, DRIVETRAIN_DISTANCE_D);
+    m_frontRight.Config_kF(0, DRIVETRAIN_DISTANCE_F);
+
+    //set acceleration and velocity based on real units per 100 ms
+    m_frontLeft.ConfigMotionCruiseVelocity(DistanceToTicks(CRUISE_VELOCITY * 100_ms), 10);
+    m_frontLeft.ConfigMotionAcceleration(DistanceToTicks(CRUISE_ACCELERATION * 1_s * 100_ms ), 10);
+    m_frontRight.ConfigMotionCruiseVelocity(DistanceToTicks(CRUISE_VELOCITY * 100_ms), 10);
+    m_frontRight.ConfigMotionAcceleration(DistanceToTicks(CRUISE_ACCELERATION * 1_s * 100_ms ), 10);
+    smoothing = 0;
+    AdjustSmoothing(0);
+
+    /* Zero the sensor */
+    m_frontLeft.SetSelectedSensorPosition(0, 0, 10);
+    m_frontRight.SetSelectedSensorPosition(0, 0, 10); 
+}
+
+void DriveSubsystem::ZeroEncoders(){
+    m_frontLeft.SetSelectedSensorPosition(0, 0, 10);
+    m_frontRight.SetSelectedSensorPosition(0, 0, 10);
+}
+
+bool DriveSubsystem::IsStopped(){
+    int threshold = 100;
+    return std::abs(m_frontLeft.GetSelectedSensorVelocity(0)) < threshold && std::abs(m_frontRight.GetSelectedSensorVelocity(0)) < threshold;
+}
+
+//average of both wheels in terms of error
+units::meter_t DriveSubsystem::ClosedLoopError(){
+    double average = (m_frontLeft.GetClosedLoopError(0) + m_frontRight.GetClosedLoopError(0)) / 2.0;
+    return TicksToDistance(average);
+}
+
+//called periodically
+void DriveSubsystem::RunMotionMagic(units::meter_t distance){
+    double ticksDistance = DistanceToTicks(distance);
+    m_frontLeft.Set(ControlMode::MotionMagic, ticksDistance);
+    m_frontRight.Set(ControlMode::MotionMagic, ticksDistance);
+}
+
+units::meter_t DriveSubsystem::GetAverageDistance(){
+    return TicksToDistance(m_frontLeft.GetSelectedSensorPosition(0) + m_frontRight.GetActiveTrajectoryPosition(0)) / 2.0;
+}
+
+units::meter_t DriveSubsystem::TicksToDistance(double ticks){
+    return ticks / TICKS_PER_ROTATION  / WHEEL_TO_FALCON_RATIO * wpi::numbers::pi * WHEEL_DIAMETER;
+}
+
+double DriveSubsystem::DistanceToTicks(units::meter_t distance){
+    return distance / wpi::numbers::pi / WHEEL_DIAMETER * TICKS_PER_ROTATION * WHEEL_TO_FALCON_RATIO;
+}
+
+void DriveSubsystem::AdjustSmoothing(int x){
+    smoothing += x;
+    if(smoothing < 0){
+        smoothing = 0;
+    }else if(smoothing > 8){
+        smoothing = 8;
+    }
+    // will also probably want visual display of current smoothing
+    m_frontLeft.ConfigMotionSCurveStrength(smoothing, 0);
+    m_frontRight.ConfigMotionSCurveStrength(smoothing, 0);
+}
