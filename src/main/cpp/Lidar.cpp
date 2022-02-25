23f581078ef431f76e4750c10f18dc9f54c1a302
Tam Nguyen <tam.nguyen@asdnh.org>
2022-02-24 20:02:42 -0500

    start thread function, and couts lidar distances

--- src/main/cpp/Lidar.cpp
+++ src/main/cpp/Lidar.cpp
@@ -2,14 +2,28 @@
 #include <thread>
 #include <string>
 #include <frc/SerialPort.h>
+#include <iostream>
 
-char buffer[MAX_BYTE_COUNT];
+
 
 void Lidar::readThread() {
     while (true) {
         //reads lidar code from arduino
+        int readerToInt;
+        frc::SerialPort::Port kOnboard;
+        char buffer[MAX_BYTE_COUNT];
+
+        frc::SerialPort lidarSerialPort(115200, kOnboard);
+
         lidarSerialPort.Read(buffer, MAX_BYTE_COUNT);
 
-        readerToInt[MAX_BYTE_COUNT] = std::atoi(buffer);
+        readerToInt = std::atoi(buffer);
+        
+        std::cout << readerToInt;
     }
 }
+
+void Lidar::StartLidar() {
+    std::thread lidarThread(readThread);
+}
+
--- src/main/include/Lidar.h
+++ src/main/include/Lidar.h
@@ -4,8 +4,8 @@
 // https://first.wpi.edu/FRC/roborio/release2015/docs/cpp/classSerialPort.html#a86c989d7946cc35008fe12d5b9976057 
 
 class Lidar {   
+    public: 
+        void StartLidar();
     private: 
-        int readerToInt[MAX_BYTE_COUNT];
-        frc::SerialPort lidarSerialPort{115200, kOnboard};
-        void readThread();
+        static void readThread();
 };
